c311ba1007194de29751812fd52a162addab1b4a
zhang513 <657697084@qq.com>
2022-05-17 23:58:44 +0800

    no message

--- spiders/test.py
+++
@@ -1,4 +0,0 @@
-from dbs.pipelines import MongoPipeline
-
-for pro_info in MongoPipeline('products').find({}):
-    MongoPipeline('products').unset_item({'_id': pro_info['_id']}, {'pro_name': '', 'pro_td': '', 'pro_detail_html': ''})
